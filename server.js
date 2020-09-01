6494b9a5d7cedc6ed8f9aab4ccdccb34738f2a70
Alhasid <othonielj.rodriguez@gmail.com>
2020-09-01 14:22:29 -0400

    Non recurring URL Shortener

--- server.js
+++ server.js
@@ -1,34 +1,45 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const ShortUrl = require('./models/shortUrl')
-const app = express()
+const express = require('express');
+const mongoose = require('mongoose');
+const ShortUrl = require('./models/shortUrl');
+var { nanoid } = require("nanoid");
+const dotenv = require('dotenv');
+const app = express();
 
-mongoose.connect('mongodb://localhost/urlShortener', {
+dotenv.config();
+
+mongoose.connect(process.env.DB_CONNECT, {
   useNewUrlParser: true, useUnifiedTopology: true
-})
+});
 
-app.set('view engine', 'ejs')
-app.use(express.urlencoded({ extended: false }))
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: false }));
 
 app.get('/', async (req, res) => {
-  const shortUrls = await ShortUrl.find()
-  res.render('index', { shortUrls: shortUrls })
-})
+
+  res.render('index', {shortUrl1:{full: 0, short: 0, clicks: 0}});
+});
 
 app.post('/shortUrls', async (req, res) => {
-  await ShortUrl.create({ full: req.body.fullUrl })
+  var fullcheck = await ShortUrl.findOne({ full: req.body.fullUrl });
 
-  res.redirect('/')
-})
+  if(fullcheck == null ){
+    await ShortUrl.create({ full: req.body.fullUrl });
+    var added = await ShortUrl.findOne({ full: req.body.fullUrl });
+    res.render('index', { shortUrl1: added });
+  }else{
+    res.render('index', { shortUrl1: fullcheck });
+  }
+  
+});
 
 app.get('/:shortUrl', async (req, res) => {
-  const shortUrl = await ShortUrl.findOne({ short: req.params.shortUrl })
-  if (shortUrl == null) return res.sendStatus(404)
+  const shortUrl1 = await ShortUrl.findOne({ short: req.params.shortUrl });
+  if (shortUrl1 == null) return res.sendStatus(404);
 
-  shortUrl.clicks++
-  shortUrl.save()
+  shortUrl1.clicks++
+  shortUrl1.save();
 
-  res.redirect(shortUrl.full)
-})
+  res.redirect(shortUrl1.full);
+});
 
-app.listen(process.env.PORT || 5000);
+app.listen(process.env.PORT || 2000, () => console.log('server up!'));
